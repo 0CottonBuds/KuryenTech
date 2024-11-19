9b538bfc850667749e362dce8212b6398a287afd
Rishaye Melad <ragm923@gmail.com>
2024-11-19 23:39:30 +0800

    redirect login to profile

---
+++ scripts/login.js
@@ -0,0 +1,30 @@
+import { AuthUser } from '/scripts/firebase.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+
+  document.getElementById("login-button").addEventListener('click', async (event) => {
+    event.preventDefault(); // Prevent form from reloading the page
+
+    console.log("submit")
+    // Get username and password values
+    const username = document.getElementById('username-input').value;
+    const password = document.getElementById('password-input').value;
+
+    try {
+      const isAuthenticated = await AuthUser(username, password);
+
+      if (isAuthenticated) {
+        alert('Login successful!');
+        // Redirect or perform other actions after successful login
+        window.location.href = '/pages/profile.html';
+      }
+      else {
+        alert('Invalid username or password');
+      }
+    } catch (error) {
+      console.error('Error during authentication:', error);
+      alert('An error occurred. Please try again later.');
+    }
+  });
+});
